e1ee3a94523030596a30489098a5805683901ba3
JayisSuckAtjava <zavacxva@gmail.com>
2022-05-18 09:35:02 +0900

    이재범커밋

---
+++ PRO/src/main/java/COM/DDD/CHO/jay/test.java
@@ -0,0 +1,13 @@
+package COM.DDD.CHO.jay;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class test {
+
+	@RequestMapping("/")
+	public void test() {
+		
+	}
+}
